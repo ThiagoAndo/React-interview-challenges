3f81051fff9010e59dac50ae5a4aa06f7fbb7ee1
ThiagoAndo <ando.thiago@gmail.com>
2024-12-20 09:48:54 +0000

    Controled InputValidation

--- src/Challenges/unsolved/State-Management-Hooks/InputValidation.jsx
+++ src/Challenges/unsolved/State-Management-Hooks/InputValidation.jsx
@@ -13,21 +13,78 @@ import React from "react";
 import styles from "./inputValidation.module.css";
 import { forwardRef, useState, useRef, useEffect } from "react";
 
-const FormInput = forwardRef((props, ref) => {
-  return <input ref={ref} className={props.thisStyle} {...props} />;
-});
+// const FormInput = forwardRef((props, ref) => {
+//   return <input ref={ref} className={props.thisStyle} {...props} />;
+// });
 
+// const InputValidationUI = () => {
+//   const emailRef = useRef();
+//   const passwordRef = useRef();
+//   const [err, setErr] = useState([]);
+//   const errArr = [];
+//   const handleSubmit = () => {
+//     emailRef.current.value.trim().length === 0 && errArr.push("Email is required") ;
+//     passwordRef.current.value.trim().length === 0&& errArr.push("Password is required");
+//     !emailRef.current.value.trim().includes("@") && errArr.push("Email is wromg") ;
+//     !emailRef.current.value.trim().length <5 && errArr.push("Password must be at leadt 5 characters") ;
+//     errArr.length > 0 && setErr(errArr);
+//   };
+
+//   useEffect(() => {
+//     const time = setTimeout(() => {
+//       setErr([]);
+//     }, 3000);
+
+//     return () => {
+//       clearTimeout(time);
+//     };
+//   }, [err]);
+
+//   return (
+//     <div className={styles.container}>
+//       <h1 className={styles.title}>Form Validation</h1>
+//       <FormInput
+//         type="email"
+//         placeholder="Enter your email"
+//         thisStyle={styles.inputField}
+//         ref={emailRef}
+//       />
+//       <FormInput
+//         type="password"
+//         placeholder="Enter your password"
+//         thisStyle={styles.inputField}
+//         ref={passwordRef}
+//       />
+//       <button className={styles.button} onClick={handleSubmit}>
+//         Submit
+//       </button>
+//       <div className={styles.validationMessages}>
+//         {err && err.map((msg) => <p key={msg}>{msg}</p>)}
+//       </div>
+//     </div>
+//   );
+// };
 const InputValidationUI = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  const [inp, setInp] = useState({ email: "", password: "" });
   const [err, setErr] = useState([]);
   const errArr = [];
+
+  const handleChange = (e) => {
+    setInp((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
   const handleSubmit = () => {
-    emailRef.current.value.trim().length === 0 && errArr.push("Email is required") ;
-    passwordRef.current.value.trim().length === 0&& errArr.push("Password is required");
-    !emailRef.current.value.trim().includes("@") && errArr.push("Email is wromg") ;
-    !emailRef.current.value.trim().length <5 && errArr.push("Password must be at leadt 5 characters") ;
-    errArr.length > 0 && setErr(errArr);
+    inp.email.trim().length === 0 && errArr.push("Email is required");
+    inp.password.trim().length === 0 && errArr.push("Password is required");
+    if (errArr.length > 0) {
+      setErr(errArr);
+      return;
+    }
+    !inp.email.trim().includes("@") && errArr.push("Email is wrong");
+    !inp.password.trim().length < 5 &&
+      errArr.push("Password must be at leadt 5 characters");
   };
 
   useEffect(() => {
@@ -43,17 +100,21 @@ const InputValidationUI = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Form Validation</h1>
-      <FormInput
+      <input
         type="email"
+        name="email"
         placeholder="Enter your email"
-        thisStyle={styles.inputField}
-        ref={emailRef}
+        className={styles.inputField}
+        defaultValue={inp.email}
+        onChange={handleChange}
       />
-      <FormInput
+      <input
         type="password"
+        name="password"
         placeholder="Enter your password"
-        thisStyle={styles.inputField}
-        ref={passwordRef}
+        className={styles.inputField}
+        defaultValue={inp.password}
+        onChange={handleChange}
       />
       <button className={styles.button} onClick={handleSubmit}>
         Submit
@@ -64,5 +125,4 @@ const InputValidationUI = () => {
     </div>
   );
 };
-
 export default InputValidationUI;
